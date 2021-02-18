0bff8c73abdad51395fc27f97b4d3d6e1edb6e45
Francisco Ramirez Felipe <framirez.ics@gmail.com>
2021-02-18 12:49:54 -0600

    cors enabled

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
+app.use(
+  cors({
+    origin: true,
+  })
+);
 app.use(express.json());
-app.use('/api/postulantes', require('./routes/postulantes'));
-app.use('/api/auth-postulantes', require('./routes/auth-postulantes'));
-app.use('/api/empresas', require('./routes/empresas'));
-app.use('/api/auth-empresas', require('./routes/auth-empresas'));
+app.use("/api/postulantes", require("./routes/postulantes"));
+app.use("/api/auth-postulantes", require("./routes/auth-postulantes"));
+app.use("/api/empresas", require("./routes/empresas"));
+app.use("/api/auth-empresas", require("./routes/auth-empresas"));
 app.listen(process.env.PORT);
