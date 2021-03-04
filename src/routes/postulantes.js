f5977d055ec6fab12bbf10f4300a4ffd7470dfe7
GibbyStack <carlosorozco4565@gmail.com>
2021-03-03 22:53:34 -0600

    Endpoint para completar el perfil fixes #7

--- src/routes/postulantes.js
+++ src/routes/postulantes.js
@@ -2,6 +2,10 @@
 const Router = require('express');
 //Se requieren los metodos de los postulantes del archivo postulantes.js
 const { getPostulantes, getPostulante, updatePostulante, deletePostulante } = require('../bml/controllers/postulantes');
+//Se requiere el uso de check de express-validator
+const { check } = require('express-validator');
+//Se requiere la funcion validFields del archivo validar-campos.js
+const { validFields } = require('../bml/middlewares/validar-campos');
 //Se requiere la funcion para validar el refreshToken y el token
 const { validJWT } = require('../bml/middlewares/validar-jwt');
 
@@ -16,9 +20,22 @@ router.get('/:id',
     getPostulante
 );
 
+//Ruta para completar y actualizar el perfil del usuario
+router.put('/update', [
+    validJWT,
+    //Se valida cada uno de los parametros para actualizar al postulante
+    check('nombre', 'El nombre es obligatorio').notEmpty(),
+    check('apellido_paterno', 'El apellido paterno es obligatorio').notEmpty(),
+    check('apellido_materno', 'El apellido materno es obligatorio').notEmpty(),
+    check('fecha_nacimiento', 'La fecha de nacimiento es obligatoria').isDate(),
+    check('sexo', 'El sexo es obligatorio').notEmpty(),
+    //Se utiliza la funcion para validar los campos para dejar o no pasar la peticion
+    validFields
+    //Se manda llamar la funcion para actualizar al postulante
+], updatePostulante);
+
 //Rutas de nuestros postulantes
 router.get('/', validJWT, getPostulantes);
-router.put('/:id', updatePostulante);
 router.delete('/:id', deletePostulante);
 
 //Exportamos el router
