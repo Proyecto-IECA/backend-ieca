b80378fa609a06803554a4aa8fb2f3734be53231
GibbyStack <carlosorozco4565@gmail.com>
2021-05-12 17:13:11 -0500

    Retornar el conteo de calificaciones dfixes #94

--- src/apiServices/Comentarios/dao.js
+++ src/apiServices/Comentarios/dao.js
@@ -4,7 +4,7 @@ const Usuario = require("../../services/mysql/models/Usuarios");
 const getComentariosEmpresa = async(id_usuario) => {
     return new Promise((resolve, reject) =>
         Usuario.findByPk(id_usuario, {
-            attributes: ["nombre", "pagina_web", "calificacion"],
+            attributes: ["nombre", "pagina_web", "calificacion", "numero_calificaciones"],
             include: {
                 model: Comentario
             }
