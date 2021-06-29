26ec8bc67edab7e690078bfcecac4b733b36fd84
GibbyStack <carlosorozco4565@gmail.com>
2021-06-29 11:50:10 -0500

    Correcion de Vinculacion IECA

--- src/apiServices/Vinculacion/dao.js
+++ src/apiServices/Vinculacion/dao.js
@@ -1,13 +1,13 @@
 const conString = require("../../config/bd-ieca");
 const mysql = require('mysql2');
-const connection = mysql.createConnection(conString);
 
 
 const querySingle = async(CURP) => {
     return new Promise((resolve, reject) => {
+        const connection = mysql.createConnection(conString);
         connection.query("SELECT * FROM alumnos WHERE curp = '" + CURP + "'", (err, rows) => {
-            console.log(err);
             if (err) return reject(err);
+            connection.end();
             return resolve(rows)
         });
     });
